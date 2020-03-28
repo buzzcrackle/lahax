c7b38b764a8dfafc97ad65b22072de3d616d2344
Jesse Liang <jesseliang@ymail.com>
2020-03-27 22:14:50 -0700

    doesnt work

--- android/lahacks/app/src/main/java/com/example/lahacks/MainActivity.java
+++ android/lahacks/app/src/main/java/com/example/lahacks/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.lahacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(this, TrackerService.class);
+        startService(serviceIntent);
+
     }
 }
